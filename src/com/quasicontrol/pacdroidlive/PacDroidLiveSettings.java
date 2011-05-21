4e83cd17c2c8e1c43e5a4907ba3b693b9662c842
Stuart Layton <slayton@mit.edu>
2011-05-21 00:37:28 -0400

    added a few features as well as the ability to donate

--- src/com/quasicontrol/pacdroidlive/PacDroidLiveSettings.java
+++ src/com/quasicontrol/pacdroidlive/PacDroidLiveSettings.java
@@ -2,6 +2,7 @@ package com.quasicontrol.pacdroidlive;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -17,6 +18,8 @@ public class PacDroidLiveSettings extends PreferenceActivity {
 		//setContentView(R.xml.prefs);
 		 Preference email_dev = this.findPreference("email_dev");
 	     email_dev.setOnPreferenceClickListener(new EmailPrefClickListener(this.getApplicationContext()));
+	     Preference donate = this.findPreference("donate");
+	     donate.setOnPreferenceClickListener(new DonatePrefClickListener(this.getApplicationContext()));
 	}
 
 	/**
@@ -47,9 +50,26 @@ public class PacDroidLiveSettings extends PreferenceActivity {
 			
 			Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
 			emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, "support@quasicontrol.com");		
-			emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Pac Droid: Bug Report/Feature Request");
+			emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Fruit Hunter: Bug Report/Feature Request");
 			emailIntent.setType("plain/text");
 			startActivity(emailIntent);
+
+			return true;
+		}
+    }
+	private class DonatePrefClickListener implements OnPreferenceClickListener
+	{
+    	Context c = null;
+    	public DonatePrefClickListener(Context c)
+    	{
+    		this.c = c;
+    	}
+		public boolean onPreferenceClick(Preference preference) {
+			
+			String url = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=5WJNUY8BWJJ7G&lc=US&item_name=Quasicontrol%20Applications&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted";
+			Intent i = new Intent(Intent.ACTION_VIEW);
+			i.setData(Uri.parse(url));
+			startActivity(i);
 
 			return true;
 		}
